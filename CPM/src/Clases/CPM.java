9fb9fbf54dd96396e23d7ab9f5d9b61630af3717
JuanSW18 <juan.eneque@unmsm.edu.pe>
2017-07-15 21:36:33 -0500

    CPMok

--- CPM/src/Clases/CPM.java
+++ CPM/src/Clases/CPM.java
@@ -40,7 +40,7 @@ public class CPM {
      * @param actividades
      * @param tabla
      */
-    public void FuncionCPM(TextField actividades, TableView<Actividad> tabla) throws java.lang.NullPointerException{
+    public void FuncionCPM(TextField actividades, TableView<Actividad> tabla) {
         
         // número de puestos de trabajo
         int N = Integer.parseInt(actividades.getText());
@@ -52,27 +52,34 @@ public class CPM {
         // crear red
         DigrafoAristaPonderada G = new DigrafoAristaPonderada(2*N + 2);
         for (int i = 0; i < N; i++) {
+            System.out.println("ITERACION " + i);
             //double duracion = StdIn.readDouble();
             double dur = Double.parseDouble(tabla.getItems().get(i).getTiempo());
+            System.out.println("2.-el valor de duracion es: " + dur);
             G.agregarArista(new AristaDirigida(inicio, i, 0.0));
             G.agregarArista(new AristaDirigida(i+N, fin, 0.0));
             G.agregarArista(new AristaDirigida(i, i+N,    dur));
             
             String pres = tabla.getItems().get(i).getPrecedentes();
-            String[] lista = pres.split(",");
-            
-            int[] lista_pre = new int[lista.length];
-            for(int j=0; j<lista_pre.length; j++){
-                lista_pre[j]=Integer.parseInt(lista[j]);
-                System.out.println(lista_pre[j]);
-            }
-            // restricciones de precedencia
-            int M = StdIn.readInt();
-            int m = lista_pre.length;
-            for (int j = 0; j < m; j++) {
-                //int precedente = StdIn.readInt();
-                //G.agregarArista(new AristaDirigida(N+i, precedente, 0.0));
-                G.agregarArista(new AristaDirigida(N+i, lista_pre[j], 0.0));
+            if(!pres.equals("")){
+                String[] lista = pres.split(",");
+
+                int[] lista_pre = new int[lista.length];
+                for (int j = 0; j < lista_pre.length; j++) {
+                    lista_pre[j] = Integer.parseInt(lista[j]);
+                    System.out.print(lista_pre[j] + " ");
+                }
+                System.out.println("");
+                // restricciones de precedencia
+                //int M = StdIn.readInt();
+                int m = lista_pre.length;
+                System.out.println("3.-el valor de M es: " + m);
+                for (int j = 0; j < m; j++) {
+                    //int precedente = StdIn.readInt();
+                    //G.agregarArista(new AristaDirigida(N+i, precedente, 0.0));
+                    System.out.println("4.-el valor de precedente es: " + lista_pre[j]);
+                    G.agregarArista(new AristaDirigida(N + i, lista_pre[j], 0.0));
+                }
             }
         }
 
